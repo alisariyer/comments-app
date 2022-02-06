48b443e7a3724bfcb7cb734eef1ff9af5bd72ed9
Ali Sariyer <alisariyer@gmail.com>
2022-02-06 10:16:32 +0000

    Finish modal styling, optimize button element stlye in base.css, p element line-height is add in base.css instead of each class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./styles/base.css";
 import "./styles/card.css";
+import "./styles/modal.css";
 import CardContainer from "./components/CardContainer";
-import AddComment from './components/AddComment'
+import AddComment from "./components/AddComment";
+import Modal from "./components/Modal";
 
 export default function App() {
   return (
-  <div className="container">
+    <div className="container">
+      <Modal />
       <CardContainer />
       <AddComment />
-  </div>
+    </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Modal() {
     return (
         <div class="modal">
-
+            <section>
+                <h3>Delete comment</h3>
+                <p>Are you sure want to delete this comment? This will remove the comment and can't be undone.</p>
+                <div class="modal-footer">
+                    <button type="button" className="btn-no">NO, CANCEL</button>
+                    <button type="button" className="btn-yes">YES, DELETE</button>
+                </div>
+            </section>
         </div> 
     )
 }

e1fd118cc94e183ca3c1d3a9031c8616158ee11c
Ali Sariyer <alisariyer@gmail.com>
2022-02-07 20:37:57 +0000

    Add vote changing feature for first level comments in App.js file

--- src/App.js
+++ src/App.js
@@ -6,34 +6,61 @@ import "./styles/media.css";
 import CardContainer from "./components/CardContainer";
 import AddComment from "./components/AddComment";
 import Modal from "./components/Modal";
-import defaultData from './data.json'
+import defaultData from "./data.json";
 
 export default function App() {
-
   const getData = () => {
-    const data = JSON.parse(localStorage.getItem('data'));
-    if (!data) { 
-        localStorage.setItem('data', JSON.stringify(defaultData))
-        return defaultData;
-    } 
+    const data = JSON.parse(localStorage.getItem("data"));
+    if (!data) {
+      localStorage.setItem("data", JSON.stringify(defaultData));
+      return defaultData;
+    }
     return data;
-  }
+  };
 
   const [data, setData] = useState(getData());
   const handleData = (data) => {
     setData(data);
-    localStorage.setItem('data', JSON.stringify(data))
-  }
+    localStorage.setItem("data", JSON.stringify(data));
+  };
 
   const handleVote = (vote, id) => {
-    console.log(vote, 'id' , id);
-  }
+    console.log(vote, "id", id);
+    const comments = data.comments;
+
+    // Use for loop after equality break loop for performance issue in case of bigger arrays
+    for (let i = 0; i < comments.length; i++) {
+      if (comments[i].id === id) {
+
+        // If score is 0 and user click minus button do not make changing
+        if (comments[i].score === 0 && vote === -1) break;
+
+        // Otherwise change score value
+        comments[i] = {
+          ...comments[i],
+          'score': comments[i].score + vote
+        }
+
+        // After changing comments set in data object
+        setData((data) => {
+          return {
+            ...data,
+            'comments': comments,
+          };
+        });
+      }
+    }
+  };
 
   return (
     <div className="container">
       {/* <Modal /> */}
-      <CardContainer data={data} handleData={handleData} handleVote={handleVote}/>
-      <AddComment data={data} handleData={handleData}/>
+      <CardContainer
+        data={data}
+        handleData={handleData}
+        handleVote={handleVote}
+      />
+      <AddComment data={data} handleData={handleData} />
     </div>
   );
 }
